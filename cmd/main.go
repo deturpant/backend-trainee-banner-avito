3c51c8c7a2d3fa186f60a6531770f0b464276173
deturpant <vlad3032.kozlov@yandex.ru>
2024-04-07 10:34:28 +0300

    Logger init

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,41 @@ package main
 import (
 	"backend-trainee-banner-avito/internal/config"
 	"fmt"
+	"log/slog"
+	"os"
+)
+
+const (
+	LocalEnv = "local"
+	DevEnv   = "dev"
+	ProdEnv  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoadConfig()
 	fmt.Println(cfg.Env)
-
+	log := setupLogger(cfg.Env)
+	log.Info("Application started....", slog.String("env", cfg.Env))
+	log.Debug("Debug messages are active")
 	// TODO : LOGGER  : slog
 	// TODO : STORAGE : postgresql
 	// TODO : ROUTER  : chi, chi-render
 	// TODO : SERVER
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case LocalEnv:
+		log = slog.New(slog.NewTextHandler(os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug}))
+	case DevEnv:
+		log = slog.New(slog.NewJSONHandler(os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelDebug}))
+	case ProdEnv:
+		log = slog.New(slog.NewJSONHandler(os.Stdout,
+			&slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
